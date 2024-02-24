772f863988f9a16f34cb039174f78f8b4c82d785
Omar Rizwan <omar@omar.website>
2024-02-24 12:43:28 -0500

    mobile: QR code decoding works

--- mobile.js
+++ mobile.js
@@ -1,19 +1,28 @@
 function runMobile() {
-  async function getCamera() {
-    // Prefer camera resolution nearest to 1280x720 - just as calibdb does
-    let constraints = { audio: false, video: { width: 1280, height: 720, facingMode: "environment", resizeMode: "none" } }
-
-    let mediaStream = await navigator.mediaDevices.getUserMedia(constraints)
-    const track = mediaStream.getVideoTracks()[0]
-    const cfg = track.getSettings()
-
-    return [track.label, [cfg.width, cfg.height]]
-  }
-  getCamera().then(args => {
-    document.getElementById("out").innerText = JSON.stringify(args);
-  });
+  const video = document.createElement("video");
+  document.body.appendChild(video);
+  const qrScanner = new QrScanner(video,
+                                  result => console.log('decoded qr code:', result));
+  qrScanner.start();
+  // 
+  // 
+  //   
+  //   async function getCamera() {
+  //     // Prefer camera resolution nearest to 1280x720 - just as calibdb does
+  //     let constraints = { audio: false, video: { width: 1280, height: 720, facingMode: "environment", resizeMode: "none" } }
+  // 
+  //     let mediaStream = await navigator.mediaDevices.getUserMedia(constraints)
+  //     const track = mediaStream.getVideoTracks()[0]
+  //     const cfg = track.getSettings()
+  // 
+  //     return [track.label, [cfg.width, cfg.height]]
+  //   }
+  //   getCamera().then(args => {
+  //     document.getElementById("out").innerText = JSON.stringify(args);
+  //   });
 
   // Run QR code detector, localize-ish, produce cone
+  
 
   // Get accelerometer
 
